e078ea5db2bf6e90a5c981ab7317a7b9e27f772d
Zoe Yu <zoeyu97@gmail.com>
2019-05-01 18:56:59 -0400

    oops forgot to push

--- app/src/index.js
+++ app/src/index.js
@@ -1,15 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.scss';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Recorder from './components/Recorder';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      loading: false,
+      audioText: 'click the microphone to record some audio!',
+      audioBlob: null,
+    };
+  }
+
+  handleLoading = () => {
+    if (this.state.loading) {
+      return <CircularProgress color="secondary" />;
+    }
+    return <Recorder microphoneStarted={this.microphoneStarted} sendAudioBlob={this.getAudioBlob} />;
+  }
+
+  getAudioBlob = (blob) => {
+    this.setState({
+      audioBlob: blob,
+    });
+
+    this.sendRequest();
+  }
+
+  microphoneStarted = () => {
+    this.setState({
+      audioText: 'listening...',
+    });
+  }
+
+  sendRequest = () => {
+    console.log('sending a request to IBM...');
   }
 
   render() {
     return (
-      <div />
+      <div className="container">
+        {this.handleLoading()}
+      </div>
     );
   }
 }
--- server/src/server.js
+++ server/src/server.js
@@ -33,7 +33,7 @@ app.post('/', upload.single('file'), (req, res, next) => {
     body: fs.createReadStream(req.file.path),
     auth: {
       user: 'apikey',
-      pass: '<YOUR API KEY>',
+      pass: 'Fii2oeyy4d3SFdZ_3vkPUxUIqK_EZ3DWkpGqnxjFz4gA',
     },
   }, (error, response, body) => {
     if (error) {
